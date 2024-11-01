fc688c56d73922e038c9b74f409fdc18ded0abec
gunelediyarova <gunelbaghishova@gmail.com>
2024-11-01 14:46:59 -0400

    Class commit

---
+++ src/day01/HelloWorld.java
@@ -0,0 +1,11 @@
+package day01;
+
+public class HelloWorld {
+
+    public static void main (String[] arg){
+
+        System.out.println("Hello World from Universe!");
+
+
+    }
+}
---
+++ src/day03_comments_escape_sequence/Goal.java
@@ -0,0 +1,13 @@
+package day03_comments_escape_sequence;
+
+import java.sql.SQLOutput;
+
+public class Goal {
+    public static void main (String [] arg){
+
+        System.out.println("Understand better Java");
+        System.out.println("Do all tasks.");
+        System.out.println("Find a good job!");
+
+    }
+}
